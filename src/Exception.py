53bff2962fddd330b0d53ca34d4fd863b6df8ac4
shivayyBaj <shiveshbajpai830>
2026-03-11 19:30:00 +0530

    eda and problem statement

--- src/Exception.py
+++ src/Exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 logging.basicConfig(level=logging.INFO)
 
@@ -22,4 +22,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e, sys)
